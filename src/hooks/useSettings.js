3e64dc22fb79bb3061fb430078637466723a2a56
nowscott <nowscott@qq.com>
2024-07-08 02:49:53 +0800

    把设置存储到缓存封装成hook

---
+++ src/hooks/useSettings.js
@@ -0,0 +1,22 @@
+// src/hooks/useSettings.js
+import { useState, useEffect } from 'react';
+
+const useSettings = (initialSettings) => {
+    const [settings, setSettings] = useState(() => {
+        const savedSettings = localStorage.getItem('settings');
+        return savedSettings ? JSON.parse(savedSettings) : initialSettings;
+    });
+
+    const updateSetting = (stateKey, value) => {
+        const newSettings = {
+            ...settings,
+            [stateKey]: value,
+        };
+        setSettings(newSettings);
+        localStorage.setItem('settings', JSON.stringify(newSettings));
+    };
+
+    return [settings, updateSetting];
+};
+
+export default useSettings;
